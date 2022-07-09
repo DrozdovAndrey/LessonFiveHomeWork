1358a250ec03a2f0d8ffb0076f2e48644655f188
Andrey Drozdov <drozdov_andrey@list.ru>
2022-07-09 11:07:40 +0300

    task38 done

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -10,23 +10,24 @@ namespace HomeWork5
     {
         static void Main(string[] args)
         {
-            double[] array = new double [new Random().Next(4, 11)];
+            double[] array = new double[new Random().Next(4, 11)];
             FillArray(array);
             PrintArray(array);
             Console.WriteLine();
-
-            Console.WriteLine($"Разница между максимальным ({GetMax(array)}) и минимальным ({GetMin(array)}) значением в массиве: {GetMax(array) - GetMin(array)}");
+            Console.WriteLine($"Минимальное значение: {GetMin(array)}");
+            Console.WriteLine($"Максимальное значение: {GetMax(array)}");
+            Console.WriteLine($"Разница между максимальным и минимальным значением в массиве: {GetDifference(array)}");
 
         }
         static void FillArray(double[] array)
         {
             for (int i = 0; i < array.Length; i++)
             {
-                array[i] = Math.Round(new Random().NextDouble() * (100 - (-100)) + (-100),2);
-                   //+ new Random().Next(-99, 100);
+                array[i] = Math.Round(new Random().NextDouble() * (100 - (-100)) + (-100), 2);
+                //+ new Random().Next(-99, 100);
             }
         }
-        static void PrintArray(double [] array)
+        static void PrintArray(double[] array)
         {
             for (int i = 0; i < array.Length; i++)
             {
@@ -35,23 +36,34 @@ namespace HomeWork5
         }
         static double GetMin(double[] array)
         {
-            double min = 0;
+            double min = array[0];
             for (int i = 1; i < array.Length; i++)
             {
-                min = array[0];
-                if (array[i] < min) min = array[i];  
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
             }
+            
             return min;
         }
         static double GetMax(double[] array)
         {
-            double max = 0;
+            double max = array[0];
             for (int i = 1; i < array.Length; i++)
             {
-                max = array[0];
-                if (array[i] > max) max = array[i];  
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
             }
+            
             return max;
+        }
+        static double GetDifference(double[] array)
+        {
+            double difference = GetMax(array) - GetMin(array);
+            return difference;
         }
     }
 }
